278ea9c34e8eb80fe1fedfae1fc1f69903661240
Greg Reda <gjreda+github@gmail.com>
2013-04-29 22:30:29 -0500

    use bs4 methods

--- get_sandwiches.py
+++ get_sandwiches.py
@@ -5,7 +5,7 @@
 # Source: http://www.chicagomag.com/Chicago-Magazine/November-2012/Best-Sandwiches-Chicago/
 # Blog: http//www.gregreda.com/2013/04/29/more-web-scraping-with-python/
 
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 from urllib2 import urlopen
 import csv
 
@@ -16,7 +16,7 @@ soup = BeautifulSoup(urlopen(base_url).read())
 sammies = soup.find_all("div", "sammy")
 sammy_urls = [div.a["href"] for div in sammies]
 
-with open("/src-best-sandwiches.tsv", "w") as f:
+with open("data/src-best-sandwiches.tsv", "w") as f:
     fieldnames = ("rank", "sandwich", "restaurant", "description", "price",
                     "address", "phone", "website")
     output = csv.writer(f, delimiter="\t")
@@ -26,17 +26,16 @@ with open("/src-best-sandwiches.tsv", "w") as f:
         url = url.replace("http://www.chicagomag.com", "")  # inconsistent URL
         page = urlopen("http://www.chicagomag.com{0}".format(url))
         soup = BeautifulSoup(page.read()).find("div", {"id": "sandwich"})
-
-        rank = soup.find("div", {"id": "sandRank"}).render_contents().strip()
-        sandwich = soup.h1.render_contents().strip().split("<br />")[0]
-        restaurant = soup.h1.span.render_contents()
-        description = soup.p.render_contents().strip()
-        addy = soup.find("p", "addy").em.render_contents().split(",")[0].strip()
+        rank = soup.find("div", {"id": "sandRank"}).encode_contents().strip()
+        sandwich = soup.h1.encode_contents().strip().split("<br />")[0]
+        restaurant = soup.h1.span.encode_contents()
+        description = soup.p.encode_contents().strip()
+        addy = soup.find("p", "addy").em.encode_contents().split(",")[0].strip()
         price = addy.partition(" ")[0].strip()
         address = addy.partition(" ")[2].strip()
-        phone = soup.find("p", "addy").em.render_contents().split(",")[1].strip()
+        phone = soup.find("p", "addy").em.encode_contents().split(",")[1].strip()
         if soup.find("p", "addy").em.a:
-            website = soup.find("p", "addy").em.a.render_contents()
+            website = soup.find("p", "addy").em.a.encode_contents()
         else:
             website = ""
 
